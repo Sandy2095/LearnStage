068a5e57b78bf7d013fe07c4c07f05a1bf89116f
E <santhosh.e@hpe.com>
2018-05-22 19:26:02 +0530

    first commit

---
+++ Python/Python_Less.py
@@ -0,0 +1,77 @@
+a = 10
+print (a)
+pi = 3.17
+print (pi)
+name = "Sandy"
+print (name)
+
+##Simultanous Assignent 
+x = y = 10
+print (x,y)
+print(id(x)) ###### id to check object memory 
+print(id(y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##
+##
+##### Integer
+##int_var = 10  #### int_var is integer variable
+##float_var = 3.14 ## float_var is float variable
+##comp_var = 3 + 2j  ### comp_var is complex variable where 3 is real part & 2j is imaginary part
+##
+###### String
+##name_str = "Sandy" ##### single quatotion & double quatotion
+##print(name_str[0]) #####String indexing prints 0th index of name_str variable
+##print (name_str[0:3]) #### STring slicing
+##
+##print (ord("c"))
+##print (chr(98))
+##
+##print ("sandy" == "candy") ## String comparision 
+##
+##for each in "string": ####### Iteration using String 
+##    print (each) 
+##
+######### List
+##    
+##list_var = [1,2,3,4]
+##
+##print (list_var)
+##
+##print (list_var[0]) ####### Accessing particular elemnts from list using index 
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
